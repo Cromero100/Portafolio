4eb0ca90ac9a1ca28460a65e6f89e2ca0420915d
Cristian Romero <cfromerop@ufpso.edu.co>
2025-12-26 13:41:18 -0500

    Correccion imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './pages/Hero'
 import Projects from './pages/Projects'
-import ScrollFloat from './components/ScrollFloat'
-import TransitiownWrapper from './components/TransitionWrapper'
-import LiquidTransition from './components/LiquidTransition'
 import About from './pages/About'
 function App() {
 
